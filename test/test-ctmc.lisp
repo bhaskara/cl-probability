0805411452ebb0990a397530b1c119e9914db460
Bhaskara Marthi <bhaskara@willowgarage.com>
2010-02-18 13:19:59 -0800

    added intervals, exponential dists

--- test/test-ctmc.lisp
+++ test/test-ctmc.lisp
@@ -1,5 +1,5 @@
 (defpackage :test-ctmc
-    (:use :cl :cl-test :ctmc :cl-utils))
+    (:use :cl :cl-test :ctmc :cl-utils :cl-probability))
 
 (in-package :test-ctmc)
 
@@ -8,6 +8,8 @@
 (defparameter a '(a b c))
 (defparameter m (make-instance 'continuous-time-markov-chain :q q :alphabet a :init-dist d))
 (defparameter m2 (make-instance 'continuous-time-markov-chain :q q :init-dist d))
+(defparameter q3 #2A((-1.0 .5 .5) (0.0 -1.0 1.0) (.001 0.0 -.001)))
+(defparameter m3 (make-instance 'continuous-time-markov-chain :q q3 :alphabet a))
 
 (defparameter h (loop repeat 10000 collect (sample-jump m 'a)))
 (let ((*tol* .1))
@@ -18,5 +20,4 @@
  100000 .1
  (nth-value 1 (sample-jump m2 1))
  '((0 . .2) (2 . .8)))
- 
-		  
+
